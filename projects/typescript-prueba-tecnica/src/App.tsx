767b10208fdb08bb3915336855a2e809fa180b4d
JL-OLEMAR <joseluis19963@gmail.com>
2023-10-25 02:01:25 -0500

    feat: Point 1 ✅

--- projects/typescript-prueba-tecnica/src/App.tsx
+++ projects/typescript-prueba-tecnica/src/App.tsx
@@ -1,9 +1,23 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import { type User } from './types.d'
 
 export default function App() {
+  const [users, setUsers] = useState<User[]>([])
+
+  useEffect(() => {
+    fetch('https://randomuser.me/api?results=100')
+      .then((res) => res.json())
+      .then((data) => { setUsers(data.results) })
+      .catch((err) => { console.log(err) })
+  }, [])
+
   return (
     <div className='App'>
       <h1>Prueba técnica</h1>
+      {
+        JSON.stringify(users)
+      }
     </div>
   )
 }
